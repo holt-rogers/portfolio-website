2dcef0acf36ea3f8050cdf63b32c7803a0b8a9fa
Ethan Rogers <75224466+holt-rogers@users.noreply.github.com>
2025-11-14 21:44:14 -0500

    finished project layout

--- src/scripts/project-data.js
+++ src/scripts/project-data.js
@@ -1,10 +1,10 @@
 export const projectTitles = [
-    { id: 1, title: 'Navy Missile Optimization', summary: 'A program that optimizes the ideal stage size for a multi-stage missile.'},
-    { id: 2, title: 'Motion Controlled Mouse', summary: 'A computer mouse controlled by rotation using Arduino.'},
+    { id: 1, title: 'Navy Missile Optimization', summary: 'A program that optimizes the ideal stage size for a multi-stage missile.BOOOOM BOI'},
+    { id: 2, title: 'Motion Controlled Mouse', summary: 'A computer mouse controlled by rotational movement.'},
     { id: 3, title: 'PenGuin', summary: 'A note taking app designed for STEM problem solving.'},
 ];
 
-export default projectTitles;
+
 
 export const projectFiles = [
     "Project 1",
--- src/scripts/projects.js
+++ src/scripts/projects.js
@@ -1,13 +1,13 @@
-import projectTitles from "./project-data.js"
+import {projectTitles } from "./project-data.js"
+import { projectFiles } from "./project-data.js";
 
 
 export function createProjectList(){
 
-    
-    
     projectTitles.forEach((project, index) => {
         const button = document.createElement('button');
         button.classList.add('projectbutton');
+        button.onclick = openProject(project.id)
 
         const title = document.createElement('p');
         title.textContent = project.title;
@@ -23,5 +23,9 @@ export function createProjectList(){
 
         document.getElementById("projectList").append(button);
     });
+}
 
+function openProject(id){
+    const data = projectFiles[id];
+    const descriptionDiv = document.getElementById("projectDescription")
 }
